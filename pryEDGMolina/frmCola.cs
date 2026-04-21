019b0796d3e9a59581edf1e2ba1fb531d3b82edc
guadamolina06 <guadaaalupemolinaaa@gmail.com>
2026-04-21 11:50:20 -0300

    completar formularios

--- pryEDGMolina/frmCola.cs
+++ pryEDGMolina/frmCola.cs
@@ -21,5 +21,43 @@ namespace pryEDGMolina
         {
             dgvCola.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
         }
+        clsCola fila = new clsCola();
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            clsNodos n = new clsNodos();
+            n.Codigo = Convert.ToInt32(txtCodigo.Text);
+            n.Nombre = txtNombre.Text;
+            n.Tramite = txtTramite.Text;
+           
+            fila.Agregar(n);
+            fila.Recorrer(dgvCola);
+            fila.Recorrer(lstCola);
+            
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtTramite.Clear();
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (fila.Primero != null)
+            {
+                lblCodigoE.Text = fila.Primero.Codigo.ToString();
+                lblNombreE.Text = fila.Primero.Nombre;
+                lbltramiteE.Text = fila.Primero.Tramite;
+                fila.eliminar();
+                fila.Recorrer(dgvCola);
+                fila.Recorrer(lstCola);
+                fila.Recorrer();
+            }
+            else
+            {
+                lblCodigoE.Text = "";
+                lblNombreE.Text = "";
+                lbltramiteE.Text = "";
+            }
+        }
     }
 }
